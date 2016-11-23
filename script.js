61282ef27f3d1410c2606711c10535542033d909
Ben <benj.kn@gmail.com>
2016-11-23 16:57:13 +0200

    still with mistakes

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 var app = angular.module("appName", []);
 
+app.factory('redditShit', function () {
+  var uselessListOfPosts= [];
+
+  return {lalala : uselessListOfPosts};
+})
+
+app.controller('weLostCtrl', function ($scope, redditShit) {
+  $scope.posts = redditShit.lalala;
+
+  $scope.postText = function () {
+    $scope.posts.push({name: $scope.newPost, likes: 0});
+    $scope.newPost = '';
+  };
+
+  $scope.addLike = function (i) {
+   redditShit.lalala[i].likes += 1;
+  };
+})
